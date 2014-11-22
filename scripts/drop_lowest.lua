98a8ee51a7ad091ece9f70a6e19698372fabc6ae
christopher:4 <christopher:4@firmy-primary.tfs.local>
2014-11-22 18:56:47 +0000

    I have implemented the functionality for the Drop Lowest module working. Though it isn't exactly what I was hoping it would be at the moment, it is functional. I have also added several of the feats from the Complete Adventurer to the Basic Rules module.
    git-tfs-id: [http://localhost:8080/tfs/DMFirmyProjects]$/Dungeons and Dragons Projects/Theon Campaign Setting/Fantasy_Grounds/data/extensions/Drop Lowest;C524

--- scripts/drop_lowest.lua
+++ scripts/drop_lowest.lua
@@ -2,6 +2,7 @@
 -- Initialization
 function onInit()
     Comm.registerSlashHandler("rolld", processRoll);
+	GameSystem.actions["rolld"] = { bUseModStack = false };
 	ActionsManager.registerResultHandler("rolld", onRoll);
 
 	ActionsManager.print("Drop Lowest Init");
@@ -16,16 +17,41 @@ end
 function processRoll(sCommand, sParams)
 	local rRoll = {};
 	rRoll.sType = "rolld";
-	rRoll.aDice = {  };
-	rRoll.nMod = 0;						
-	ActionsManager.performAction(nil, nil, rRoll);	
+	rRoll.aDice = {"d6", "d6", "d6", "d6"};
+	rRoll.nMod = 0;
+	rRoll.sUser = User.getUsername();
+	ActionsManager.roll(nil, nil, rRoll);
+	
 end
 
-
-
 function onRoll(rSource, rTarget, rRoll)
-	rRoll.aDice = { "d6", "d6", "d6", "d6" };
-	local rThrow = ActionsManager.buildThrow(rSource, vTargets, rRoll, false);
-	Comm.throwDice(rThrow);
+	
+	local i = 1;
+	local lowest = 999;
+	local lowestIndex = 0;
+	while i < 5 do
+		if rRoll.aDice[i].result < lowest then
+			lowest = rRoll.aDice[i].result;
+			lowestIndex = i;
+		end
+		i = i + 1;
+	end
 
+	i = 1;
+	local dice = {};
+	for _,v in ipairs(rRoll.aDice) do
+		if(i ~= lowestIndex) then
+			table.insert(dice,v);
+		end
+		i = i + 1;
+	end
+
+	-- Build the basic message to deliver
+	local rMessage = ChatManager.createBaseMessage(rSource, rRoll.sUser);
+	rMessage.type = rRoll.sType;
+	rMessage.text = rMessage.text .. "A " .. lowest .. " was dropped";
+	rMessage.dice = dice;
+	rMessage.diemodifier = rRoll.nMod;
+
+	Comm.deliverChatMessage(rMessage);
 end
